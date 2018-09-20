0a3ae772b6d8e24a2e6276d1fa29fd159ca7b090
MaxiPineker <maxi.pineker@gmail.com>
2018-09-20 11:32:55 +0200

    Blockheader is edited

--- hello_world.c
+++ hello_world.c
@@ -1,3 +1,18 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: < class: 2Dhif>
+ * ---------------------------------------------------------
+ * Exercise Number: 0
+ * Title:			<hello_world>
+ * Author:			<Maxi Pineker>
+ * ----------------------------------------------------------
+ * Description:
+ * <some brief description, what the program does: example:
+ * Calculates a pyramid of numbers, i.e., it multiplies a big
+ * integer by the number 2, its result by 3, etc. and then
+ * returns to the original value by subsequently dividing,
+ * again starting by 2, 3, etc.>
+ * ----------------------------------------------------------
+ */
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
